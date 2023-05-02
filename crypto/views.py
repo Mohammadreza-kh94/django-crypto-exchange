3f3006527e1893182c6fc6b62b3787e6c8c4d9fb
Mohammadreza-kh94 <f69.khoobbakht@gmail.com>
2023-05-02 10:06:51 +0330

    update: change crypto  permission to `AllowAny`

--- crypto/views.py
+++ crypto/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from .models import Crypto
 from .serializers import CryptoSerializer
@@ -9,10 +9,10 @@ from .serializers import CryptoSerializer
 class CryptoListAPIView(generics.ListAPIView):
     queryset = Crypto.objects.all()
     serializer_class = CryptoSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
 
 
 class CryptoDetailAPIView(generics.RetrieveAPIView):
     queryset = Crypto.objects.all()
     serializer_class = CryptoSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
